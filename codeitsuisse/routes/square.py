78b59b4957a9c6b3776b26f530c6f7caf5018578
Wei Lun <weilunn.97@gmail.com>
2020-09-26 12:55:17 +0800

    Update square.py

--- codeitsuisse/routes/square.py
+++ codeitsuisse/routes/square.py
@@ -3,16 +3,16 @@ import logging
 
 from flask import request;
 
-from codeitsuisse import app;
+# from codeitsuisse import app;
 
 logger = logging.getLogger(__name__)
 
 
-@app.route('/square', methods=['POST'])
-def evaluate_square():
-    data = request.get_json()
-    logging.info("data sent for evaluation {}".format(data))
-    inputValue = data.get("input")
-    result = inputValue * inputValue
-    logging.info("My result :{}".format(result))
-    return json.dumps(result)
+# @app.route('/square', methods=['POST'])
+# def evaluate_square():
+#     data = request.get_json()
+#     logging.info("data sent for evaluation {}".format(data))
+#     inputValue = data.get("input")
+#     result = inputValue * inputValue
+#     logging.info("My result :{}".format(result))
+#     return json.dumps(result)
