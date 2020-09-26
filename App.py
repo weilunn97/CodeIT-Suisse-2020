53efb7ef1ab8a7d9c6c85813a36bd3b601040707
Wei Lun <weilunn.97@gmail.com>
2020-09-26 11:37:19 +0800

    Updated requirements.txt

--- App.py
+++ App.py
@@ -1,22 +1,23 @@
 import logging
 import socket
-from codeitsuisse import  app
+
+from codeitsuisse import app
+
 logger = logging.getLogger(__name__)
+
 
 @app.route('/', methods=['GET'])
 def default_route():
-    return "Python Template";
+    return "Python Template"
 
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
 formatter = logging.Formatter(
-        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
-
-
 
 if __name__ == "__main__":
     logging.info("Starting application ...")
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     sock.bind(('localhost', 0))
     port = sock.getsockname()[1]
     sock.close()
-    app.run(port=port)
+    app.run(port=5000)
